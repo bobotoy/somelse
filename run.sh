24e59bf3c1edbe14b0a428428a0d3292a041e9e4
bobotoy <32072974+bobotoy@users.noreply.github.com>
2021-09-23 00:54:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+   
 #!/bin/sh
 
 # Download and install XRay
@@ -6,64 +7,11 @@ curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XT
 unzip /tmp/xray/xray.zip -d /tmp/xray
 install -m 755 /tmp/xray/xray /usr/local/bin/xray
 
+# 自动加载配置
+curl -L -H "Cache-Control: no-cache" -o /etc/config.json https://raw.githubusercontent.com/bobotoy/somelse/config.json
 # Remove temporary directory
 rm -rf /tmp/xray
 
-# XRay new configuration
-install -d /usr/local/etc/xray
-cat << EOF > /usr/local/etc/xray/config.json
-{
-    "inbounds": [
-        {
-            "port": 8080,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "f86886e7-a5cb-4ad3-8891-d140c1ec3902",
-                        "level": 0
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws",
-                "wsSettings": {
-                    "path": "/xray"
-                }
-            }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "settings": {
-                "domainStrategy": "UseIPv4"
-            },
-            "tag": "ip-out"
-        }
-    ],
-    "dns": {
-        "servers": [
-            "https+local://1.1.1.1/dns-query",
-            "8.8.8.8",
-            "9.9.9.9",
-            "208.67.222.222"
-        ],
-        "queryStrategy": "UseIPv4"
-    },
-    "routing": {
-        "domainStrategy": "IPIfNonMatch",
-        "rules": [
-            {
-                "type": "field",
-                "outboundTag": "ip-out",
-                "network": "tcp,udp"
-            }
-        ]
-    }
-}
-EOF
-
 # Run XRay
-/usr/local/bin/xray run -config /usr/local/etc/xray/config.json
+/usr/local/bin/xray run -config /etc/config.json
+
